308873cec1f1f4d624a80b6a20da1ec144ff5177
isosea <isokazu.psy@gmail.com>
2023-03-24 11:08:20 +0900

    get list

--- front/lib/screens/attraction_list_screen.dart
+++ front/lib/screens/attraction_list_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:front/screens/route_screen.dart';
+import 'package:front/services/place_api_service.dart';
 import 'package:front/widgets/bottom_app_bar.dart';
 
 class AttractionListScreen extends StatelessWidget {
@@ -8,45 +10,55 @@ class AttractionListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Attraction List Screen'),
-      ),
-      bottomNavigationBar: ListScreenBottomBar(
-        buttonText: 'Optimize Itinerary',
-        nextPage: RouteScreen(),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: 4,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: Image.network(
-                      'https://picsum.photos/seed/$index/400/300',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Title $index',
-                      style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
+        appBar: AppBar(
+          title: const Text('Attraction List Screen'),
         ),
-      ),
-    );
+        bottomNavigationBar: ListScreenBottomBar(
+          buttonText: 'Optimize Itinerary',
+          nextPage: RouteScreen(),
+        ),
+        body: FutureBuilder<List<Map<String, dynamic>>>(
+          future: getPlaces(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+            if (snapshot.hasData) {
+              List<Map<String, dynamic>> places = snapshot.data!;
+              return Container(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: places.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: Image.network(
+                              'https://picsum.photos/seed/$index/400/300',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              places[index]['name'].toString(),
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
--- front/lib/services/place_api_service.dart
+++ front/lib/services/place_api_service.dart
@@ -12,3 +12,18 @@ Future<void> addPlace(String name) async {
     print(error);
   }
 }
+
+Future<List<Map<String, dynamic>>> getPlaces() async {
+  final dio = Dio();
+  final url = 'http://127.0.0.1:8000/api/places/';
+
+  try {
+    final Response response = await dio.get(url);
+    final List<Map<String, dynamic>> data =
+        List<Map<String, dynamic>>.from(response.data);
+    return data;
+  } catch (error) {
+    print(error);
+    throw Exception('Failed to get places');
+  }
+}
